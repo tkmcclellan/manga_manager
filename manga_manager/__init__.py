cca16827d19b35fd905708f025bf97287e6afff9
Tyler McClellan <35702927+tkmcclellan@users.noreply.github.com>
2021-04-10 19:21:33 -0500

    Bump version: 0.1.0 → 0.1.0

--- manga_manager/__init__.py
+++ manga_manager/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Tyler Kyle McClellan"""
 __email__ = 'the8bitgamer11@gmail.com'
-__version__ = '0.1.0'
+__version__ = '__version__ = '0.1.0''
--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/tkmcclellan/manga_manager',
-    version='0.1.0',
+    version='version='0.1.0'',
     zip_safe=False,
 )
